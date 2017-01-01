741b12c086e6e479c753853ab7481fdf42f4b8a9
vigneshm <vig.mrgn@gmail.com>
2016-12-31 20:13:25 -0800

    Adding first exercise

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # elm-simple-exercises
 
-A set of simple exercises to get familiar with [elm](http://elm-lang.org) programming language.
+A set of simple exercises to get familiar with [elm](http://elm-lang.org) programming language. To start with the exercises, clone this experiment to your local machine and follow these steps.
+
+1. `cd elm-simple-exercises`
+2. `elm package install`
+3. `elm reactor`
+4. Open the given url in your browser
+5. Click on a specific exercise and solve them until all tests pass
---
+++ src/Exercise01.elm
@@ -0,0 +1,45 @@
+module Exercise01 exposing (main)
+
+{-
+   This exercise should help you understand how elm-tests work. Look through
+   each variable that starts with `test` and make them pass.
+-}
+
+import Expect
+import Test exposing (concat, describe, test, Test)
+import Test.Runner.Html
+
+
+main : Test.Runner.Html.TestProgram
+main =
+    describe "Simple test"
+        [ testStringEquality
+        ]
+        |> Test.Runner.Html.run
+
+
+testStringEquality : Test
+testStringEquality =
+    test "Check string equality" <|
+        \() ->
+            -- The value on the left of `|> Expect.equal` should be equal to the
+            -- value on right. The value on the right is the expected value.
+            -- So you need to change the value on the left so that the test
+            -- passes.
+            "Foo" |> Expect.equal "Bar"
+
+
+add : Int -> Int -> Int
+add num1 num2 =
+    {-
+       The implementation of `add` function is intentionally wrong. Check the
+       `testAdd` function to see the expected result of this function.
+    -}
+    num1 + num2 + 1
+
+
+testAdd : Test
+testAdd =
+    test "add function" <|
+        \() ->
+            add 5 3 |> Expect.equal 8
